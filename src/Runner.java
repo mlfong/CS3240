87d081c81315d6e303d98f325c518e22dc7ded2c
Man Fong <mlfong@gatech.edu>
2013-03-03 01:18:39 -0500

    string validator

--- src/Runner.java
+++ src/Runner.java
@@ -6,20 +6,80 @@
 import java.util.HashMap;
 import java.util.HashSet;
 
-
 public class Runner
 {
-    public static void main(String[] args) throws BadDefinitionException
+    public static void characterClassTest() throws BadDefinitionException
     {
-        HashMap<String,HashSet<Character>> tokenClasses = 
-                ScannerGenerator.parseInput("input/mlfong_sample_input.txt");
-        for(String s : tokenClasses.keySet())
+        HashMap<String, HashSet<Character>> tokenClasses = ScannerGenerator
+                .parseInput("input/mlfong_sample_input.txt");
+        for (String s : tokenClasses.keySet())
         {
             System.out.println(s);
             System.out.print("\t");
             Util.reallyPrettyPrint(tokenClasses.get(s));
         }
+    }
 
+    public static void regexValidatorTest()
+    {
+        HashMap<String, HashSet<Character>> tokenClasses = new HashMap<String, HashSet<Character>>();
+        tokenClasses.put("$LETTER", null);
+        tokenClasses.put("$DIGIT", null);
+        tokenClasses.put("$SMALLCASE", null);
+        tokenClasses.put("$UPPERCASE", null);
+        String[] res = {
+                "$SMALLCASE ($LETTER | $DIGIT)*",
+                "$UPPERCASE ($LETTER | $DIGIT)*",
+                "",
+                "$UPPERCASE ($LETTER | $DIGIT | $NOTACLASS)*",
+                "$SMALLCASE ($LETTER | $DIGIT)*$DIGIT",
+                "$SMALLCASE ($LETTER | $DIGIT)*|",
+                "$DIGIT$DIGIT$DIGIT$DIGIT$DIGIT$DIGIT$DIGIT    $DIGIT",
+                "$DIGIT$DIGIT$DIGIT$LETTER$UPPERCASE",
+                "((()))",
+                "(()))",
+                "$UPPERCASE ($LETTER | $DIG|IT)*",
+                "$UPPERCASE ($LETTER | $DIGIT |+ $UPPERCASE)*",
+                "$UPPERCASE ($LETTER | $DIGIT | $UPPERCASE)*",
+                "+",
+                "|",
+                "(",
+                ")",
+                "*",
+                "($UPPERCASE | $UPPERCASE) $UPPERCASE* $UPPERCASE+",
+                "$UPPERCASE+", 
+                "$UPPERCASE*",
+                "$UPPERCASE",
+                "qwertyyuuidlfsdfgklsjdfg",
+                "[$UPPERCASE|$UPPERCASE]",
+                ";",
+                "'",
+                "             ",
+                "$UPPERCASE$UPPERCASE$UPPERCASE$UPPERCASE$UPPERCASE$UPPERCASE$UPPERCASE$UPPERCASE$UPPERCASE$UPPERCASE$UPPERCASE",
+                null,
+                "$DIGIT $DIG"
+        };
+        boolean[] correct_vals = {
+                true, true, false, false, true, false, true, true,false, false, false, false, true, false, false, false, false, false, true, true, true, true, false, false, false,
+                false, false, true, false, false
+        };
+        if(res.length != correct_vals.length)
+        {
+            System.err.println("You forgot a truth value!"); System.exit(-1);
+        }
+        for(int i = 0; i < res.length; i++)
+        {
+            System.out.println("Test " + (i+1) + ": " + 
+                    (TokenUtil.validateRegex(res[i], tokenClasses) == correct_vals[i] ? 
+                            "pass" : "fail")
+                    );
+        }
+
+    }
+
+    public static void main(String[] args) throws BadDefinitionException
+    {
+        regexValidatorTest();
     }
 
 }
--- src/TokenUtil.java
+++ src/TokenUtil.java
@@ -10,12 +10,93 @@ public class TokenUtil
 {
     public static int SIMP_STR = 0;
     public static int SIMP_MAP = 0;
+
+    private static final HashSet<Character> REGEX_CHARS = new HashSet<Character>()
+    {   private static final long serialVersionUID = 3983065818321009688L;
+    { this.add('('); this.add(')'); this.add('+'); this.add('*'); this.add('|'); }};
     
-    public static boolean validateRegex(String re, 
+    private static final HashSet<Character> POS_CHARS = new HashSet<Character>()
+    {   private static final long serialVersionUID = 2690642328791035372L;
+    { this.add('|'); this.add('+'); this.add('*'); }};
+    
+    public static boolean validateRegex(String re,
             HashMap<String, HashSet<Character>> characterClasses)
     {
-        
-        return false;
+        if(re == null || re.length() < 1) return false;
+        re = re.replaceAll(" ", "");
+        boolean inDollar = false;
+        StringBuilder sb = new StringBuilder();
+        int numLeftParen = 0, numClasses = 0;
+        for (int i = 0; i < re.length(); i++)
+        {
+            char c = re.charAt(i);
+            if (inDollar)
+            {
+                if (REGEX_CHARS.contains(c))
+                {
+                    if (!characterClasses.containsKey(sb.toString()))
+                        return false;
+                    inDollar = false;
+                    if (c == '(')
+                        numLeftParen++;
+                    else if (c == ')')
+                    {
+                        numLeftParen--;
+                        if (numLeftParen < 0)
+                            return false;
+                    }
+                    else if(POS_CHARS.contains(c))
+                    {
+                        if(i-1 < 0) return false;
+                        if(POS_CHARS.contains(re.charAt(i-1)))
+                            return false;
+                    }
+                    sb.delete(0, sb.length());
+                    numClasses++;
+                }
+                else
+                {
+                    if(c == '$')
+                    {
+                        if (!characterClasses.containsKey(sb.toString()))
+                            return false;
+                        sb.delete(0, sb.length());
+                    }
+                    sb.append(c);
+                }
+            }
+            else
+            {
+                if (c == '$')
+                {
+                    inDollar = true;
+                    sb.append(c);
+                }
+                else if(c == '(')
+                    numLeftParen++;
+                else if(c==')')
+                {
+                    numLeftParen--;
+                    if(numLeftParen < 0) return false;
+                }
+                else if(POS_CHARS.contains(c))
+                {
+                    if(i-1 < 0) return false;
+                    if(POS_CHARS.contains(re.charAt(i-1)))
+                        return false;
+                }
+                else
+                    return false;
+            }
+        }
+        if(sb.length() > 0)
+        {
+            if (!characterClasses.containsKey(sb.toString()))
+                return false;
+            numClasses++;
+        }
+        if(numClasses == 0) return false;
+        if(numLeftParen != 0) return false;
+        return true;
     }
-
 }
