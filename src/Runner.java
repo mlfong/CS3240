80b4893e14e1772608487cc0d089a94d02ea9a92
Man Fong <mlfong@gatech.edu>
2013-03-02 21:31:17 -0500

    update Runner.java

--- src/Runner.java
+++ src/Runner.java
@@ -12,7 +12,7 @@ public class Runner
     public static void main(String[] args) throws BadDefinitionException
     {
         HashMap<String,HashSet<Character>> tokenClasses = 
-                LexReader.parseInput("input/mlfong_sample_input.txt");
+                ScannerGenerator.parseInput("input/mlfong_sample_input.txt");
         for(String s : tokenClasses.keySet())
         {
             System.out.println(s);
