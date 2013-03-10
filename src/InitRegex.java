492ab7e924152d097970da52995d2f5e3f53dd03
Victor <victor@victor-ThinkPad-W530.(none)>
2013-03-10 18:05:39 -0400

    Fixed the bug in InitRegex.java; should be working properly now.

--- src/InitRegex.java
+++ src/InitRegex.java
@@ -10,12 +10,35 @@ public class InitRegex{
 	private static final String STAR = "*";
 	private static final String testCases[] = {"a+", "aaa+", "ab+", "abbb+", "(a)+", "(ab)+", 
 											"\\+", "\\t+", "\\)", "(ab(cd))+", "(ab(cd)*)+",
-											"a+b+c+", "(ab(cd)+)+", "abc", "a*b*c*", "(a+)+"};
-	private static final String testResults[] = {"aa*", "aaaa*", "abb*", "abbbb*", "(a)(a)*", "(ab)(ab)*", 
-											"\\+", "\\t\\t*", "\\)", "(ab(cd))(ab(cd))*", "(ab(cd)*)(ab(cd)*)*",
-											"aa*bb*cc*", "(ab(cd)(cd)*)(ab(cd)(cd)*)*", "abc", "a*b*c*", "(aa*)(aa*)*"};
+											"a+b+c+", "(ab(cd)+)+", "abc", "a*b*c*", "(a+)+", "(a)"};
+	private static final String testResults[] = {"(aa*)", "(aaaa*)", "(abb*)", "(abbbb*)", "((a)(a)*)", "((ab)(ab)*)", 
+											"(\\+)", "(\\t\\t*)", "(\\))", "((ab(cd))(ab(cd))*)", "((ab(cd)*)(ab(cd)*)*)",
+											"(aa*bb*cc*)", "((ab(cd)(cd)*)(ab(cd)(cd)*)*)", "(abc)", "(a*b*c*)", "((aa*)(aa*)*)", "(a)"};
 	public static void main(String[] args){
+		//runTest("(ab(cd)+)+", "(ab(cd)(cd)*)(ab(cd)(cd)*)*");
+		runAllTests();
+		
+	}
 
+
+	/*
+		This is the method that other people should use to run this code
+	*/
+	public static String initializeRegex(String regex){
+		return appendParens(convertPlus(regex));
+	}
+
+	//Runs a single tests
+	public static void runTest(String input, String expected){
+		String converted = initializeRegex(input);
+		System.out.println(converted);
+		if(!converted.equals(expected)){
+			System.out.println("\tERROR: Expected "+expected+" from "+input);
+		}
+	}
+
+	//Runs all the testcases
+	public static void runAllTests(){
 		if(testCases.length != testResults.length){
 			System.out.println("Error: testCases and testResults lengths do not match");
 		}
@@ -23,7 +46,7 @@ public class InitRegex{
 			for(int i = 0; i < testCases.length; i++){
 				String test = testCases[i];
 				String result = testResults[i];
-				String converted = convertPlus(test);
+				String converted = initializeRegex(test);
 				System.out.println(converted);
 				if(!result.equals(converted)){
 					System.out.println("\tERROR: Expected "+result+" from "+test);
@@ -32,11 +55,19 @@ public class InitRegex{
 		}
 	}
 
-	public static String appendParens(String regex){
+	/*
+		Private implementation code
+	*/
+
+	private static String appendParens(String regex){
+		//Check to make sure there are not already parens around the entire regex
+		if(regex.charAt(0) == LEFT_PARENS && regex.charAt(regex.length() - 1) == RIGHT_PARENS)
+			return regex;
+
 		return "("+regex+")";
 	}	
 
-	public static String convertPlus(String regex){
+	private static String convertPlus(String regex){
 		StringBuilder newString = new StringBuilder();
 		for(int regexIndex = 0; regexIndex < regex.length(); regexIndex++){
 			//Check for + AND not \+
@@ -44,7 +75,7 @@ public class InitRegex{
 				int previous = checkPrevious(regex, regexIndex);
 				if(previous == PARENS){
 					//System.out.println("PARENS!");
-					newString.append(getParensSubstring(newString.toString(), regexIndex));
+					newString.append(getParensSubstring(newString.toString()));
 				}
 				else if (previous == SLASH){
 					//System.out.println("SLASH");
@@ -91,49 +122,38 @@ public class InitRegex{
 		}
 	}
 
-
-	//If the char previous to + was a ), get the entire grouping within that paren
-	private static String getParensSubstring(String regex, int index){
+	//If the char previous to + was a ), get the entire grouping within that parens
+	//With the * appended
+	private static String getParensSubstring(String regex){
+		//Uses a stack to keep track of parens
+		//Going from right to left
+		//Pushes when it hits a right parens,
+		//Pops when it hits a left paren
+		//Ends when stack is empty
 		Stack<Character> parens = new Stack<Character>();
-		//Current index is pointed at + 
-		//So make sure to push index - 1 to get right parens
-		//And tempIndex starts the first character to the left of right parens
-		parens.push(RIGHT_PARENS);
-		int tempIndex = index - 2;
-		while(tempIndex >= 0){
-			char lastChar = regex.charAt(tempIndex);
+		int beginIndex = regex.length() - 1;
+
+	//	System.out.println("The whole regex is: "+regex);
+	//	System.out.println("The current index is: "+tempIndex);
+		while(beginIndex >= 0){
+			char lastChar = regex.charAt(beginIndex);
 			//System.out.println(lastChar);
 			if(lastChar == LEFT_PARENS){
+	//			System.out.println("LEFT_PARENS");
 				parens.pop();
 			}
 			else if(lastChar == RIGHT_PARENS){
 				parens.push(RIGHT_PARENS);
+	//			System.out.println("RIGHT_PARENS");
 			}
 
 			if(parens.empty())
 				break;
 
-			tempIndex--;
+			beginIndex--;
 		}
-		return regex.substring(tempIndex) + STAR;
+		return regex.substring(beginIndex) + STAR;
 	}
 
  
 }
-
-//(asdf)+ 
-//(asdf)(asdf)*
-//(asdfasdf(asdasdfasdf))+
-// regex:aa+bbb+
-// newstring:aaa*
-// aaa*bbbb*
-// (aaa)*
-// aaa*
-
-// (aaa)+
-
-
-
-
-// \)+
-// \)\)*
