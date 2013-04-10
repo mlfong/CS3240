9542677d6f6510833fd9ba8490b9881a3c8ed6ed
James Bowland - Gleason <jamesbg3@gmail.com>
2013-04-10 01:05:34 -0400

    Fix InputReader Boolean match

---
+++ src/rdp/tw/tw/InputToken.java
@@ -0,0 +1,25 @@
+package rdp.tw;
+
+
+public class InputToken {
+	private String value, tokenName;
+	private MyToken token;
+	
+	public InputToken(String value, MyToken token){
+		this.token = token;
+		this.value = value;
+	}
+	
+	public InputToken(String value, String tokenName){
+		this.value = value;
+		this.tokenName = tokenName;
+		this.token = null;
+	}
+	
+	public String toString(){
+		if(this.token == null){
+			return this.tokenName + " " + this.value;
+		}
+		return token.toString() + " " + value;
+	}
+}
